2a8c058402bce293d393bdb90b09692af1cefd4b
hyunwoo.park <hyunwoo.park@gcar.co.kr>
2023-10-17 13:20:02 +0900

    Initial commit

---
+++ src/main/java/file/filespring/FileSpringApplication.java
@@ -0,0 +1,13 @@
+package file.filespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/file/filespring/FileSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package file.filespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
